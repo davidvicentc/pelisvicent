3feda5e92bc23cd72a3e995906234c09bab28aa0
Omar Crespo <crespomerchano@gmail.com>
2018-10-22 12:09:34 -0400

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 const configServices = {
   apiKey: "c463d3f298d2538f726bd0913d21e501&language=es"
+  // Una API Key en github O.O para todo publico, eso es una mala practica xD
 };
 
 export default configServices;
